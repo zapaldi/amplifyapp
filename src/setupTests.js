2c30267c05a9bec0ae93b51aef89c38505a2b33a
DianaZ+1-at-952978590570 <dlzapalac@gmail.com>
2021-03-21 08:04:06 -0700

    trying to force a commit

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+// I am adding something ot try to force a commit
 import '@testing-library/jest-dom';
